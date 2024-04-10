7e752683f396c05aeb92157b78d781e4b46659d3
NikhilRoyal0 <159770607+NikhilRoyal0@users.noreply.github.com>
2024-04-10 15:51:37 +0530

    update

--- models/refer/refer_schema.js
+++ models/refer/refer_schema.js
@@ -3,8 +3,8 @@ const Joi = require("joi");
 const referSchema = Joi.object({
     user_id: Joi.number().integer().min(1).required(), 
     my_id: Joi.number().integer().min(1).required(),
-    status: Joi.string().valid('active', 'inactive', 'pending').required(),
-    created_on: Joi.date().timestamp().required() 
+    status: Joi.string().valid('completed', 'failed', 'pending').required(),
+    created_on: Joi.any().required() 
 });
 
 module.exports = {
